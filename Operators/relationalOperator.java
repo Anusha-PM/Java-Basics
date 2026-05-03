01ebe8ec1e170064a3d920158d9083ee2d28ac30
Anusha <anushapm758@gmail.com>
2026-05-03 23:44:20 +0500

    Add Operators folder

---
+++ Operators/relationalOperator.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class relationalOperator {
+    public static void main(String [] args){
+  int a = 5 , b = 10;
+
+  if(a>=b) {
+    System.out.println("Correct Relation");
+
+  }
+  else {
+    System.out.println("Not a correct relation");
+  }
+}
+}
