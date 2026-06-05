47f5c436ec4c36edabd9d7c52ccfff2408516ef4
Anusha <anushapm758@gmail.com>
2026-06-05 22:04:54 +0500

    new one

---
+++ Nestedloop/patterns.java
@@ -0,0 +1,25 @@
+public class patterns {
+    public static void main(String[] args) {
+        int n =3;
+        for(int i = 0; i<=n ;i++){
+            for(int j = 0 ;j<i;j++){
+                System.out.print(" ");
+            }
+           
+            System.out.print("*");
+            
+           System.out.println();
+        }
+        for(int i = n-1; i>=0;i--){
+             for(int j = 0 ;j<i;j++){
+                System.out.print(" ");
+            }
+           
+            System.out.print("*");
+            
+           System.out.println();
+        }
+        
+    }
+    
+}
