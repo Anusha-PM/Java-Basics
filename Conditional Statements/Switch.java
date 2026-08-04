244f73a859afb938dd755b2780e83a98c2928e36
Anusha <anushapm758@gmail.com>
2026-08-04 20:36:14 +0530

    Switch Project

---
+++ Conditional Statements/Switch.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Switch {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    int a = 10;
+    int b = 20;
+    System.out.print("Enter your character:");
+    char ch =sc.next().charAt(0);
+    switch(ch){
+        case'+': System.out.println(a+b);
+        break;
+        case'-': System.out.println(a-b);
+        break;
+        case'*': System.out.println(a*b);
+        break;
+        case'/': System.out.println(a/b);
+        break;
+        case'%': System.out.println(a%b);
+        break;
+        default : System.out.println("Invalid character");
+
+
+    }
+    }}
+    
+    
+
+
+
+
